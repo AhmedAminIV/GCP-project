8a3c5d0b4bba3d4b7a260416460831ea4b468226
Ahmed Amin <120580570+AhmedAminIV@users.noreply.github.com>
2023-11-05 00:13:17 +0200

    Delete terraform directory

--- terraform/0-provider.tf
+++
@@ -1,8 +0,0 @@
-# GCP provider
-
-provider "google" {
-  credentials = file(var.gcp_svc_key)
-  project     = var.project_id
-  region      = var.region[0]
-  zone        = var.zone
-}
--- terraform/4-Storage.tf
+++
@@ -1,6 +0,0 @@
-resource "google_artifact_registry_repository" "my-repo" {
-  location      = var.region[1]
-  repository_id = "project-repo"
-  description   = "docker repository"
-  format        = "DOCKER"
-}
